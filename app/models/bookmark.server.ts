d5b8bd5a979ab08652f63dc6b08da2a23fc3e39a
Jacek Panasiuk <panasiuk.jc@gmail.com>
2022-12-01 12:20:38 +0100

    Parse folders and files separately.
    - Add parseFolders function
    - Add generic Folder type
    - Display results side by side

--- app/models/bookmark.server.ts
+++ app/models/bookmark.server.ts
@@ -1,23 +1,15 @@
 import { load, Cheerio, Element } from "cheerio";
-import type { Bookmark, BookmarkFolder } from "./bookmark";
+import type { Bookmark, Folder } from "./bookmark";
 
 export function parseBookmarks(html: string): Bookmark[] {
   const $ = load(html);
-
-  const getParentFolderNames = ($a: Cheerio<Element>): string[] => {
-    const $dl = $a.closest("dl").prev();
-    const currentFolderName = $dl.text();
-    return $dl.length > 0
-      ? getParentFolderNames($dl).concat([currentFolderName])
-      : [];
-  };
 
   let bookmarks: Bookmark[] = [];
 
   $("a").each((_, el) => {
     let $a = $(el);
     bookmarks.push({
-      folders: getParentFolderNames($a),
+      folders: getUserFolders($a),
       title: $a.text(),
       href: $a.attr("href") || "",
       icon: $a.attr("icon") || null,
@@ -25,4 +17,39 @@ export function parseBookmarks(html: string): Bookmark[] {
   });
 
   return bookmarks;
+}
+
+export function parseFolders(html: string): Folder<Folder>[] {
+  const $ = load(html);
+
+  let folders: Folder<Folder>[] = [];
+
+  $("h3").each((_, el) => {
+    let $h3 = $(el);
+    folders.push({
+      folders: getUserFolders($h3),
+      title: $h3.text(),
+      children: [],
+    });
+  });
+
+  return folders;
+}
+
+// --- HELPERS ---
+
+/**
+ * Get list of parent folder names, in nesting order
+ * Ignore first 2 levels of nesting, since these are not user-created folders
+ */
+function getUserFolders($el: Cheerio<Element>): string[] {
+  return getParentFolderNames($el).slice(2);
+}
+
+function getParentFolderNames($el: Cheerio<Element>): string[] {
+  const $dl = $el.closest("dl").prev();
+  const currentFolderName = $dl.text();
+  return $dl.length > 0
+    ? getParentFolderNames($dl).concat([currentFolderName])
+    : [];
 }
--- app/models/bookmark.ts
+++ app/models/bookmark.ts
@@ -21,3 +21,19 @@ export type Bookmark = {
   /** Base 64 icon data */
   icon: string | null;
 };
+
+/**
+ * A flexible folder type. Can be nested.
+ * Parameter `T` specifies what may go inside the folder.
+ *
+ * @example
+ * type FolderNoChildren = Folder<never>;
+ * type FolderWithBookmarks = Folder<Bookmark>;
+ * type FolderNestedNoFiles = Folder<Folder>;
+ * type FolderNestedWithBookmarks = Folder<Folder<Bookmark> | Bookmark>;
+ */
+export type Folder<T = unknown> = {
+  folders: string[];
+  title: string;
+  children: T[];
+};
