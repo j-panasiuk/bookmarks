09e9a16cf1597e8ce069b185c772fb0004df56b7
Jacek Panasiuk <panasiuk.jc@gmail.com>
2022-12-07 12:43:40 +0100

    Update layout styles

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -14,34 +14,31 @@ export function Layout({ header, nav, main, aside }: Props) {
         className={c(
           "flex flex-1 items-center justify-between",
           "h-app/header pr-4 mx-auto",
-          "border-b border-gray-200",
-          "todo"
+          "border-b border-gray-200"
         )}
       >
         {header}
       </div>
 
-      <div className={c("mx-auto h-app/main", "lg:grid lg:grid-cols-12")}>
-        <div className={c("hidden", "lg:col-span-3 lg:block", "xl:col-span-3")}>
+      <div className={c("h-app/main mx-auto", "md:grid md:grid-cols-12")}>
+        <aside className={c("hidden", "xl:block xl:col-span-3")}>
           <nav
             aria-label="Sidebar"
             className={c(
               "sticky top-0",
-              "h-max-full overflow-y-auto",
-              "py-3 px-3.5",
+              "h-max-full py-3 px-3.5 overflow-y-auto",
               "border-r border-gray-200"
             )}
           >
             {nav}
           </nav>
-        </div>
+        </aside>
 
         <main
           className={c(
-            "h-max-full overflow-y-auto",
-            "py-3 px-3.5",
+            "h-max-full py-3 px-3.5 overflow-y-auto",
             "bg-slate-100",
-            "lg:col-span-9",
+            "md:col-span-8",
             "xl:col-span-6"
           )}
         >
@@ -50,10 +47,11 @@ export function Layout({ header, nav, main, aside }: Props) {
 
         <aside
           className={c(
-            "hidden border-l border-gray-200",
-            "bg-slate-100",
-            "lg:col-span-3 lg:block",
-            "todo"
+            "todo",
+            "hidden border-l",
+            "border-gray-200 bg-slate-100",
+            "md:block md:col-span-4",
+            "xl:col-span-3"
           )}
         >
           <div className={c("sticky top-4 space-y-4")}>{aside}</div>
