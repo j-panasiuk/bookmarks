8d24902c591cf615ff0b0d20766fcf5adce9d8d3
Jacek Panasiuk <panasiuk.jc@gmail.com>
2022-12-10 10:56:11 +0100

    Exclude vitest dependency from production build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  define: {
+    "import.meta.vitest": false,
+  },
   test: {
     includeSource: ["app/**/*.{ts,tsx}"],
   },
