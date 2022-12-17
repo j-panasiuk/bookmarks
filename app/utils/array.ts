3fb1a11145d147f41dbc65f7843cc8f0b35da216
Jacek Panasiuk <panasiuk.jc@gmail.com>
2022-12-17 09:48:02 +0100

    Update unit tests for some utils

--- app/utils/array.ts
+++ app/utils/array.ts
@@ -32,3 +32,29 @@ export function splitBy<K, T>(bucketFn: MapFn<T, K>) {
     return Array.from(buckets.values());
   };
 }
+
+if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+
+  describe("by", () => {
+    it("given an evaluate function returns a function which sorts arrays", () => {
+      expect(["aa", "", "a"].sort(by((s) => s.length))).toEqual([
+        "",
+        "a",
+        "aa",
+      ]);
+    });
+  });
+
+  describe("splitBy", () => {
+    it("given a bucket function returns a function which splits arrays into buckets", () => {
+      expect(splitBy((s: string) => s.length)(["aa", "a", "b"])).toEqual([
+        ["aa"],
+        ["a", "b"],
+      ]);
+      expect(
+        splitBy((s: string) => s.length)(["aa", "", "a", "b", "bb", "a"])
+      ).toEqual([["aa", "bb"], [""], ["a", "b", "a"]]);
+    });
+  });
+}
--- app/utils/classes.ts
+++ app/utils/classes.ts
@@ -1,3 +1,26 @@
 export function classes(...names: (string | undefined)[]): string {
-  return names.filter(Boolean).join(" ");
+  return names.filter(isNonEmpty).map(trim).join(" ");
+}
+
+function isNonEmpty(val: string | undefined): val is string {
+  return Boolean(val);
+}
+
+function trim(val: string): string {
+  return val.trim();
+}
+
+if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+
+  describe("classes", () => {
+    it("joins a list of non-empty strings", () => {
+      expect(classes("")).toBe("");
+      expect(classes("a", "b")).toBe("a b");
+      expect(classes("a", undefined, "", "b")).toBe("a b");
+    });
+    it("trims returned string", () => {
+      expect(classes(" a ", " b ")).toBe("a b");
+    });
+  });
 }
