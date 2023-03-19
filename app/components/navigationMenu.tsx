5cbf92a37f32f10899bd44e693b1ce808cfe5e42
Jacek Panasiuk <panasiuk.jc@gmail.com>
2023-03-19 21:26:38 +0100

    Fix z-index issue on NavigationMenu

--- app/components/navigationMenu.tsx
+++ app/components/navigationMenu.tsx
@@ -45,7 +45,7 @@ export function NavigationMenu({ fileName, fileNames }: Props) {
       >
         <Menu.Items
           className={c(
-            "absolute left-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5",
+            "absolute left-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5",
             "focus:outline-none"
           )}
         >
