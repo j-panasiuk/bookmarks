01d4e91cf3af26a2d8b1af8bd55077dc4d1e1297
Jacek Panasiuk <panasiuk.jc@gmail.com>
2023-03-20 10:09:30 +0100

    Reorganize folders navigation
    - Each folder can be expanded/collapsed independently now
    - Get rid of code repetition between top-level and nested folders

--- app/components/folders.tsx
+++ app/components/folders.tsx
@@ -1,13 +1,13 @@
+import { FolderIcon, FolderOpenIcon } from "@heroicons/react/24/outline";
 import {
-  ChevronDownIcon,
-  ChevronUpIcon,
-  FolderIcon,
-  FolderOpenIcon,
-} from "@heroicons/react/24/outline";
-import { useState } from "react";
+  type ButtonHTMLAttributes,
+  type DetailedHTMLProps,
+  type MouseEventHandler,
+  useState,
+} from "react";
 import type { Folder } from "~/bookmarks.types";
-import { isInside, isSameAs } from "~/bookmarks.utils";
-import { classes as c } from "~/utils/classes";
+import { getItemId, isSameAs } from "~/bookmarks.utils";
+import { c } from "~/utils/classes";
 
 type Props = {
   folders: Folder<Folder>[];
@@ -19,96 +19,130 @@ export function FoldersNav(props: Props) {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <>
+    <div>
       <div className={c("flex")}>
-        <button
-          className={c(
-            "mr-2.5 mb-px cursor-default select-none items-center rounded-md py-1 px-1.5 text-sm font-semibold text-slate-500 hover:bg-slate-50",
-            "flex flex-1",
-            !props.currentFolder ? "bg-slate-100 text-slate-600" : undefined
-          )}
+        <FolderIconButton
+          onClick={() => setIsExpanded((exp) => !exp)}
+          isOpen={isExpanded}
+        />
+        <FolderLabelButton
           onClick={() => props.setCurrentFolder()}
-        >
-          <FolderOpenIcon className={c("mr-2.5 h-5 w-5")} />
-          <h2 className="my-0.5 h-5">FOLDERS</h2>
-        </button>
-        <button onClick={() => setIsExpanded(true)}>
-          <ChevronDownIcon
-            aria-hidden
-            className={c("h-5 w-5 stroke-slate-400 hover:stroke-slate-600")}
-          />
-        </button>
-        <button onClick={() => setIsExpanded(false)}>
-          <ChevronUpIcon
-            aria-hidden
-            className={c("h-5 w-5 stroke-slate-400 hover:stroke-slate-600")}
-          />
-        </button>
+          label={"Bookmarks bar"}
+          isCurrent={props.currentFolder === undefined}
+        />
       </div>
 
-      <Folders level={0} isExpanded={isExpanded} {...props} />
-    </>
+      <Folders level={0} {...props} />
+    </div>
   );
 }
 
-interface TreeProps extends Props {
+interface FoldersProps extends Props {
   level: number;
-  isExpanded: boolean;
 }
 
-export function Folders({
-  level,
-  isExpanded,
-  folders,
-  setCurrentFolder,
-  currentFolder,
-}: TreeProps) {
+function Folders(props: FoldersProps) {
   return (
-    <ul className={c(level === 0 ? undefined : "ml-4")}>
-      {folders.map((folder, index) => {
-        const isOpen = isFolderOpen(folder, currentFolder);
-        const isSelected = isFolderSelected(folder, currentFolder);
-        const Icon = isOpen ? FolderOpenIcon : FolderIcon;
-
-        return (
-          <li key={folder.title + index}>
-            <div
-              onClick={() => setCurrentFolder(folder)}
-              className={c(
-                "mb-px flex select-none items-center rounded-md py-1 px-1.5",
-                isSelected ? "bg-slate-100 text-slate-600" : "hover:bg-slate-50"
-              )}
-            >
-              <Icon
-                className={c(
-                  "mr-2.5 h-5 w-5 flex-none stroke-2",
-                  isSelected ? "stroke-slate-600" : "stroke-slate-400"
-                )}
-              />
-              {folder.title}
-            </div>
-            {isExpanded || isOpen ? (
-              <Folders
-                level={level + 1}
-                isExpanded={isExpanded}
-                folders={folder.children as Folder<Folder>[]}
-                setCurrentFolder={setCurrentFolder}
-                currentFolder={currentFolder}
-              />
-            ) : null}
-          </li>
-        );
-      })}
+    <ul className={c(props.level === 0 ? undefined : "ml-4")}>
+      {props.folders.map((folder) => (
+        <Folder key={getItemId(folder)} folder={folder} {...props} />
+      ))}
     </ul>
+  );
+}
+
+interface FolderProps extends FoldersProps {
+  folder: Folder<Folder>;
+}
+
+function Folder({
+  level,
+  folder,
+  currentFolder,
+  setCurrentFolder,
+}: FolderProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const isSelected = isFolderSelected(folder, currentFolder);
+
+  return (
+    <li key={getItemId(folder)}>
+      <div className={c("flex")}>
+        <FolderIconButton
+          onClick={() => setIsOpen((open) => !open)}
+          isOpen={isOpen}
+        />
+        <FolderLabelButton
+          onClick={() => setCurrentFolder(folder)}
+          label={folder.title}
+          isCurrent={isSelected}
+        />
+      </div>
+      {isOpen ? (
+        <Folders
+          level={level + 1}
+          folders={folder.children as Folder<Folder>[]}
+          setCurrentFolder={setCurrentFolder}
+          currentFolder={currentFolder}
+        />
+      ) : null}
+    </li>
+  );
+}
+
+interface FolderIconButtonProps
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  isOpen: boolean;
+}
+
+function FolderIconButton({ isOpen, ...buttonProps }: FolderIconButtonProps) {
+  const Icon = isOpen ? FolderOpenIcon : FolderIcon;
+
+  return (
+    <button {...buttonProps}>
+      <Icon
+        className={c(
+          "mx-2 h-5 w-5 stroke-2",
+          "hover:bg-slate-700",
+          isOpen ? "stroke-slate-100" : "stroke-slate-400"
+        )}
+      />
+    </button>
+  );
+}
+
+interface FolderLabelButtonProps
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  label: string;
+  isCurrent: boolean;
+}
+
+function FolderLabelButton({
+  label,
+  isCurrent,
+  ...buttonProps
+}: FolderLabelButtonProps) {
+  return (
+    <button
+      {...buttonProps}
+      className={c(
+        "mb-px flex select-none items-center rounded-md py-1 px-2",
+        "flex flex-1",
+        isCurrent ? "bg-slate-600" : "hover:bg-slate-700"
+      )}
+    >
+      {label}
+    </button>
   );
 }
 
 function isFolderSelected(folder: Folder, currentFolder?: Folder): boolean {
   return currentFolder ? isSameAs(currentFolder)(folder) : false;
-}
-
-function isFolderOpen(folder: Folder, currentFolder?: Folder): boolean {
-  return currentFolder
-    ? isSameAs(currentFolder)(folder) || isInside(folder)(currentFolder)
-    : false;
 }
