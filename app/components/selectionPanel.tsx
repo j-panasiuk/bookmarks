029882442aa6050ce06fd1fb6c438582d97a6836
Jacek Panasiuk <panasiuk.jc@gmail.com>
2023-03-24 10:03:47 +0100

    Update selection panel and tailwind stylesheet

--- app/components/selectionPanel.tsx
+++ app/components/selectionPanel.tsx
@@ -15,6 +15,7 @@ type Props = {
 
 export function SelectionPanel({ open, setOpen, selectedBookmarks }: Props) {
   const saveToFile = useFetcher();
+  const linksFileContent = getLinksFileContent(selectedBookmarks);
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -26,48 +27,47 @@ export function SelectionPanel({ open, setOpen, selectedBookmarks }: Props) {
             <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
               <Transition.Child
                 as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enter="transform transition ease-in-out duration-400 sm:duration-500"
                 enterFrom="translate-x-full"
                 enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leave="transform transition ease-in-out duration-400 sm:duration-500"
                 leaveFrom="translate-x-0"
                 leaveTo="translate-x-full"
               >
-                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                  <div className="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl">
-                    <div className="flex min-h-0 flex-1 flex-col overflow-y-scroll py-6">
-                      <div className="px-4 sm:px-6">
-                        <div className="flex items-start justify-between">
-                          <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
-                            Panel title
-                          </Dialog.Title>
-                          <div className="ml-3 flex h-7 items-center">
-                            <button
-                              type="button"
-                              className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                              onClick={() => setOpen(false)}
-                            >
-                              <span className="sr-only">Close panel</span>
-                              <XMarkIcon
-                                className="h-6 w-6"
-                                aria-hidden="true"
-                              />
-                            </button>
-                          </div>
+                <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
+                  <div className="flex h-full flex-col bg-white shadow-xl">
+                    <section className="flex min-h-0 flex-1 flex-col overflow-y-scroll">
+                      <header className="flex h-app/header items-center justify-between px-4 sm:px-6">
+                        <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
+                          Selected bookmarks
+                        </Dialog.Title>
+                        <div className="ml-3 flex h-7 items-center">
+                          <button
+                            type="button"
+                            className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                            onClick={() => setOpen(false)}
+                          >
+                            <span className="sr-only">Close panel</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                          </button>
                         </div>
+                      </header>
+                      <div className="relative max-w-full flex-1 overflow-auto py-2 px-4 text-sm sm:px-6">
+                        <textarea
+                          className={c(
+                            "h-full w-full rounded border-indigo-400 bg-slate-50",
+                            "sm:text-sm",
+                            "hover:border-indigo-300",
+                            "focus:border-indigo-300  focus:outline-none focus:ring-0"
+                          )}
+                          defaultValue={linksFileContent}
+                        />
                       </div>
-                      <ul className="relative mt-6 flex-1 px-4 sm:px-6">
-                        {selectedBookmarks.map((bookmark) => (
-                          <li key={getItemPath(bookmark)}>{bookmark.href}</li>
-                        ))}
-                      </ul>
-                    </div>
-                    <div className="flex justify-between space-x-2 p-3">
+                    </section>
+                    <footer className="flex justify-between space-x-2 px-4 pb-6 pt-4 sm:px-6">
                       <button
                         onClick={() => {
-                          navigator.clipboard.writeText(
-                            getLinksFileContent(selectedBookmarks)
-                          );
+                          navigator.clipboard.writeText(linksFileContent);
                         }}
                         className={c(
                           "inline-flex items-center border px-3 py-2",
@@ -82,7 +82,7 @@ export function SelectionPanel({ open, setOpen, selectedBookmarks }: Props) {
                       <button
                         onClick={() =>
                           saveToFile.submit(
-                            { content: getLinksFileContent(selectedBookmarks) },
+                            { content: linksFileContent },
                             { method: "post", action: "/saveToFile" }
                           )
                         }
@@ -90,7 +90,7 @@ export function SelectionPanel({ open, setOpen, selectedBookmarks }: Props) {
                       >
                         Save to file
                       </button>
-                    </div>
+                    </footer>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
